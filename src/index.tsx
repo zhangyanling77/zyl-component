99bb02e5fdba3312a19dc5242999e9ba8bc56ea1
zhangyanling <18783226594@163.com>
2020-08-22 16:48:25 +0800

    feat: 导出组件

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 // 
 export * from './components/button';
+export * from './components/highlight';
+export * from './components/icon';
+export * from './components/avater';
 export * from './components/shared/global';
 export * from './components/shared/styles';
 export * from './components/shared/animation';
