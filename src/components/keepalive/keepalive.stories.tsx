51285e7d6952fb311c51456032b483dbbe3d2fde
zhangyanling <18783226594@163.com>
2020-09-06 14:58:38 +0800

    feat: keep-alive

---
+++ src/components/keepalive/keepalive.stories.tsx
@@ -0,0 +1,40 @@
+import React, {
+  useState,
+} from "react";
+import { KeepAlive, AliveScope } from "./index.js";
+import { withKnobs } from "@storybook/addon-knobs";
+
+export default {
+  title: "Components/Keepalive",
+  component: KeepAlive,
+  decorators: [withKnobs],
+};
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      count: {count}
+      <button onClick={() => setCount((count) => count + 1)}>add</button>
+    </div>
+  );
+}
+
+export const knobsKeepalive = () => {
+  const [show, setShow] = useState(true);
+  return (
+    <AliveScope>
+      <div>
+        <button onClick={() => setShow((show) => !show)}>Toggle</button>
+        <p>无 KeepAlive</p>
+        {show && <Counter />}
+        <p>有 KeepAlive</p>
+        {show && (
+          <KeepAlive id="Test">
+            <Counter />
+          </KeepAlive>
+        )}
+      </div>
+    </AliveScope>
+  );
+};
